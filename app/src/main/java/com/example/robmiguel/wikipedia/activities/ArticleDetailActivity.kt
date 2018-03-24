aa0dc98147dda06d69fd3f52c3ac909d60a0b881
Rob Miguel <rob.m@owletcare.com>
2018-03-24 16:18:59 -0600

    This is a new commit for what I originally planned to be amended

---
+++ app/src/main/java/com/example/robmiguel/wikipedia/activities/ArticleDetailActivity.kt
@@ -0,0 +1,16 @@
+package com.example.robmiguel.wikipedia.activities
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.example.robmiguel.wikipedia.R
+
+/**
+ * Created by robmiguel on 3/24/18.
+ */
+class ArticleDetailActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_article_detail)
+    }
+}
